347d78876b301328b9c57fa354070180787e7dc9
Tambet Arak <tambet dot arak at gmail>
2016-06-23 02:09:07 +0300

    reorganized script

--- daylen.py
+++ daylen.py
@@ -27,8 +27,113 @@ DEBUG = False
 
 
 
-def prerr(*args, **kwargs):
-    print(*args, **kwargs, file=sys.stderr)
+# Main pipeline
+
+def apply_isolines_to_image(img, date):
+    daylen = 0
+    daylen_incr = 0.5
+    last_lat = None
+
+    # If summer, start iterating from isoline 24 to ensure the polar day
+    # region is marked. Same in winter for polar night.
+
+    if (is_summer(date)):
+        daylen = 24
+        daylen_incr = -daylen_incr
+
+    # Loop over isolines and apply to image if the separation between isolines
+    # is sufficient and latitudes are not extremely polar.
+
+    while (daylen <= 24 and daylen >= 0):
+        lat = get_isoline_latitude(date, daylen)
+
+        if (not lat or (lat and
+            (last_lat and abs(last_lat - lat) < MIN_SEPARATION_DEG) or (lat > 85)
+        )):
+            daylen += daylen_incr
+            continue
+
+        color = \
+            get_hsl_gradient_point(daylen, 0, 24, GRAD_START_COLOR, GRAD_END_COLOR)
+        img = \
+            outline_latitude(img, lat, string=str(daylen), color=color, font_size=40)
+
+        prerr("{}: Applied isoline {} at latitude {:.2f}".format(date, daylen, lat))
+        last_lat = lat
+        daylen += daylen_incr
+
+    print_date(img, date, font_size=72)
+    return img
+
+def get_isoline_latitude(date, daylen):
+
+    """ Run a binary search for day length isoline latitude. Uses a
+    convergence guard to work around shortcomings of the astral module:
+    extremely short or long day isolines in polar regions will cause
+    recursion errors otherwise. In summer, high isoline values correspond to
+    high latitudes; the reverse in winter.
+    """
+
+    precision = 0.001
+    summer = is_summer(date)
+    convergence = []
+
+    def binsearch_latitude(low_lat, high_lat, target_dl):
+        mid_lat = low_lat + (high_lat - low_lat) / 2
+
+        low_dl = get_daylen_on_latitude(date, low_lat)
+        mid_dl = get_daylen_on_latitude(date, mid_lat)
+        high_dl = get_daylen_on_latitude(date, high_lat)
+
+        convergence.append(mid_lat)
+
+        if (summer and (low_dl > target_dl)):
+            return None
+        elif (not summer and (low_dl < target_dl)):
+            return None
+
+        if (
+            len(convergence) >= 3
+            and abs(convergence[-1] - convergence[-2]) <= precision
+            and abs(convergence[-2] - convergence[-3]) <= precision
+        ):
+            return mid_lat
+
+        if (abs(low_dl - target_dl) <= precision):
+            return low_lat
+        elif (abs(mid_dl - target_dl) <= precision):
+            return mid_lat
+        elif (abs(high_dl - target_dl) <= precision):
+            return high_lat
+
+        if (summer and low_dl <= target_dl and target_dl < mid_dl):
+            return binsearch_latitude(low_lat, mid_lat, target_dl)
+        elif (not summer and low_dl >= target_dl and target_dl > mid_dl):
+            return binsearch_latitude(low_lat, mid_lat, target_dl)
+        else:
+            return binsearch_latitude(mid_lat, high_lat, target_dl)
+
+    return binsearch_latitude(MIN_LATITUDE, MAX_LATITUDE, daylen)
+
+@functools.lru_cache(maxsize=2048)
+def get_daylen_on_latitude(date, lat):
+
+    """ Memoized latitude to day length function, returns day length in hours. """
+
+    loc = astral.Location()
+
+    loc.latitude = lat
+    loc.longitude = 0
+    loc.solar_depression = "civil"
+
+    try:
+        (start, end) = loc.daylight(date=date, local=False)
+    except astral.AstralError as e:
+        if (is_summer(date)):
+            return math.inf
+        return -math.inf
+
+    return (end - start).total_seconds() / 3600
 
 def outline_latitude(img, latitude, string="", font_size=20, thickness=3, color=(200, 0, 0)):
 
@@ -85,7 +190,12 @@ def outline_latitude(img, latitude, string="", font_size=20, thickness=3, color=
 
     return img
 
-def hsl_gradient(val, min_val, max_val, start_rgb, end_rgb):
+# Auxiliary routines
+
+def prerr(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+
+def get_hsl_gradient_point(val, min_val, max_val, start_rgb, end_rgb):
 
     """ Return an RGB position on a HSL gradient. RGB tuples have values in
     the range [0 .. 255].
@@ -113,30 +223,6 @@ def hsl_gradient(val, min_val, max_val, start_rgb, end_rgb):
     rgb = colorsys.hls_to_rgb(*hls)
     return (int(rgb[0] * 255), int(rgb[1] * 255), int(rgb[2] * 255))
 
-def debug_hsl_gradient(img, *extra):
-    min_val = extra[0]
-    max_val = extra[1]
-    dim = 50
-    dc = ImageDraw.Draw(img)
-
-    for i in range(min_val, max_val + 1):
-        color = hsl_gradient(i, *extra)
-        pos = i - min_val
-        dc.rectangle([0 + pos*dim, 0, 0 + pos*dim + dim, dim], fill=color)
-
-def debug_latitudes(img):
-    start = None
-
-    for i in range(MIN_LATITUDE, MIN_LATITUDE+5):
-        if (i % 5 == 0):
-            start = i
-            break
-
-    for lat in range(start, MAX_LATITUDE, 5):
-        img = outline_latitude(img, lat, str(lat))
-
-    return img
-
 def print_date(img, date, font_size=40, padding=10):
 
     """ Print the date into the upper left corner of image. """
@@ -158,111 +244,33 @@ def is_summer(date):
 
     return north_rise < south_rise
 
-@functools.lru_cache(maxsize=2048)
-def get_daylen_on_latitude(date, lat):
+# Debugging routines
 
-    """ Memoized latitude to day length function, returns day length in hours. """
+def debug_hsl_gradient(img, *extra):
+    min_val = extra[0]
+    max_val = extra[1]
+    dim = 50
+    dc = ImageDraw.Draw(img)
 
-    loc = astral.Location()
+    for i in range(min_val, max_val + 1):
+        color = get_hsl_gradient_point(i, *extra)
+        pos = i - min_val
+        dc.rectangle([0 + pos*dim, 0, 0 + pos*dim + dim, dim], fill=color)
 
-    loc.latitude = lat
-    loc.longitude = 0
-    loc.solar_depression = "civil"
+def debug_latitudes(img):
+    start = None
 
-    try:
-        (start, end) = loc.daylight(date=date, local=False)
-    except astral.AstralError as e:
-        if (is_summer(date)):
-            return math.inf
-        return -math.inf
+    for i in range(MIN_LATITUDE, MIN_LATITUDE+5):
+        if (i % 5 == 0):
+            start = i
+            break
 
-    return (end - start).total_seconds() / 3600
+    for lat in range(start, MAX_LATITUDE, 5):
+        img = outline_latitude(img, lat, str(lat))
 
-def get_isoline_latitude(date, daylen):
-
-    """ Run a binary search for day length isoline latitude. Uses a
-    convergence guard to work around shortcomings of the astral module:
-    extremely short or long day isolines in polar regions will cause
-    recursion errors otherwise. In summer, high isoline values correspond to
-    high latitudes; the reverse in winter.
-    """
-
-    precision = 0.001
-    summer = is_summer(date)
-    convergence = []
-
-    def binsearch_latitude(low_lat, high_lat, target_dl):
-        mid_lat = low_lat + (high_lat - low_lat) / 2
-
-        low_dl = get_daylen_on_latitude(date, low_lat)
-        mid_dl = get_daylen_on_latitude(date, mid_lat)
-        high_dl = get_daylen_on_latitude(date, high_lat)
-
-        convergence.append(mid_lat)
-
-        if (summer and (low_dl > target_dl)):
-            return None
-        elif (not summer and (low_dl < target_dl)):
-            return None
-
-        if (
-            len(convergence) >= 3
-            and abs(convergence[-1] - convergence[-2]) <= precision
-            and abs(convergence[-2] - convergence[-3]) <= precision
-        ):
-            return mid_lat
-
-        if (abs(low_dl - target_dl) <= precision):
-            return low_lat
-        elif (abs(mid_dl - target_dl) <= precision):
-            return mid_lat
-        elif (abs(high_dl - target_dl) <= precision):
-            return high_lat
-
-        if (summer and low_dl <= target_dl and target_dl < mid_dl):
-            return binsearch_latitude(low_lat, mid_lat, target_dl)
-        elif (not summer and low_dl >= target_dl and target_dl > mid_dl):
-            return binsearch_latitude(low_lat, mid_lat, target_dl)
-        else:
-            return binsearch_latitude(mid_lat, high_lat, target_dl)
-
-    return binsearch_latitude(MIN_LATITUDE, MAX_LATITUDE, daylen)
-
-def apply_isolines_to_image(img, date):
-    daylen = 0
-    daylen_incr = 0.5
-    last_lat = None
-
-    # If summer, start iterating from isoline 24 to ensure the polar day
-    # region is marked. Same in winter for polar night.
-
-    if (is_summer(date)):
-        daylen = 24
-        daylen_incr = -daylen_incr
-
-    # Loop over isolines and apply to image if the separation between isolines
-    # is sufficient and latitudes are not extremely polar.
-
-    while (daylen <= 24 and daylen >= 0):
-        lat = get_isoline_latitude(date, daylen)
-
-        if (not lat or (lat and
-            (last_lat and abs(last_lat - lat) < MIN_SEPARATION_DEG) or (lat > 85)
-        )):
-            daylen += daylen_incr
-            continue
-
-        color = \
-            hsl_gradient(daylen, 0, 24, GRAD_START_COLOR, GRAD_END_COLOR)
-        img = \
-            outline_latitude(img, lat, string=str(daylen), color=color, font_size=40)
-
-        prerr("{}: Applied isoline {} at latitude {:.2f}".format(date, daylen, lat))
-        last_lat = lat
-        daylen += daylen_incr
-
-    print_date(img, date, font_size=72)
     return img
+
+# Entry point
 
 def main():
     date = datetime.date(2016, 6, 1)
